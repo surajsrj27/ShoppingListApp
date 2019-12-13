a42d82ac3371d49e68c0691f334c9254c0e03cb7
suraj <srjskylark@gmail.com>
2019-12-13 09:06:27 +0530

    first commit

---
+++ models/Item.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create Schema
+const ItemShema = new Schema({
+     name: {
+         type: String,
+         required: true
+     },
+     date: {
+         type: Date,
+         default: Date.now
+     }
+});
+
+module.exports = Item = mongoose.model('item',ItemShema);
+
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
+
+const app = express();
+
+//BodyParser Middleware
+app.use(express.json());
+
+//DB Config
+const db = config.get('mongoURI');
+
+//Connect to mongoDb
+mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true})
+    .then(() => console.log('mongoDB connected!!'))
+    .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
